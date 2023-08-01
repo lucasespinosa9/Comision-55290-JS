3bcc3b2295759767fd99cc334e2ecf471f6d678c
lucas espinosa <espinosalucas1996@gmail.com>
2023-08-01 20:57:15 -0300

    segunda pre entrega

--- index.js
+++ index.js
@@ -1,68 +1,124 @@
-function presupuestoMensual() {
-    //Ingresos
-    let sueldo = parseInt(prompt("Ingrese sus ganacias del mes"))
-    //Egresos
-    let alquiler = parseInt(prompt("Ingrese cuanto abona de alquiler"))
-    let comida = parseInt(prompt("Ingrese cuanto abona en en mercaderia"))
-    let cuentas = parseInt(prompt("Ingrese cuanto abona en cuentas"))
-    let empleados = parseInt(prompt("Ingrese cuanto destina a mano de obra(empleados)"))
-
-
-    let sumaGastos = alquiler + comida + cuentas + empleados
-
-    if (sueldo == sumaGastos) {
-        console.log("Este mes no tenes ganancias ni perdidas")
-    } else if (sueldo < sumaGastos) {
-        console.log("Tenes perdidas este mes")
-    } else if (sueldo > sumaGastos) {
-        console.log("Tenes ganancias este mes")
+//Ingreso de los datos a sus variables
+function obtenerDatos(mensaje) {
+    let valor;
+    while (isNaN(valor)) {
+        valor = parseInt(prompt(mensaje));
     }
-    let diferencia = sueldo - sumaGastos
-    console.log(`La diferencia entre tu ganacia y tus gastos es de: ${diferencia}ARS`)
-    return diferencia
-
+    return valor;
 }
 
-//Bucle para indicar el mes sobre el cual hacemos el balance
-for (i = 1; i <= 12; i++) {
-    let mes = i
-    switch (mes) {
-        case i = 1:
-            console.log(`Presupuesto del mes de Enero`)
-            break
-        case i = 2:
-            console.log(`Presupuesto del mes de Febrero`)
-            break
-        case i = 3:
-            console.log(`Presupuesto del mes de Marzo`)
-            break
-        case i = 4:
-            console.log(`Presupuesto del mes de Abril`)
-            break
-        case i = 5:
-            console.log(`Presupuesto del mes de Mayo`)
-            break
-        case i = 6:
-            console.log(`Presupuesto del mes de Junio`)
-            break
-        case i = 7:
-            console.log(`Presupuesto del mes de Julio`)
-            break
-        case i = 8:
-            console.log(`Presupuesto del mes de Agosto`)
-            break
-        case i = 9:
-            console.log(`Presupuesto del mes de Septiembre`)
-            break
-        case i = 10:
-            console.log(`Presupuesto del mes de Octubre`)
-            break
-        case i = 11:
-            console.log(`Presupuesto del mes de Noviembre`)
-            break
-        case i = 12:
-            console.log(`Presupuesto del mes de Diciembre`)
-            break
-        }
-            presupuestoMensual()
+let sueldoPrimerCuatrimestre = obtenerDatos(`Ingrese sus ganacias para el primer cuatrimestre del año`);
+let alquilerPrimeraMitad = obtenerDatos(`Ingrese cuanto abona de alquiler en la primera mitad del año`);
+let comidaPrimeraMitad = obtenerDatos(`Ingrese cuanto abona de insumos en la primera mitad del año`);
+let cuentas = obtenerDatos(`Ingrese cuanto abona en cuentas mensualmente`);
+let gastosVariables = obtenerDatos(`Ingrese cuanto destina en empleados`);
+let sueldoSegundoCuatrimestre = obtenerDatos(`Ingrese sus ganancias para el segundo cuatrimestre del año`);
+let alquilerSegundaMitad = obtenerDatos(`Ingrese cuanto abona de alquiler en la segunda mitad del año`);
+let comidaSegundaMitad = obtenerDatos(`Ingrese cuanto abona de insumos en la segunda mitad del año`);
+let sueldoTercerCuatrimestre = obtenerDatos(`Ingrese sus ganancias para el tercer cuatrimestre del año`);
+let sueldoCuartoCuatrimestre = obtenerDatos(`Ingrese sus ganancias para el cuarto cuatrimestre del año`);
+
+//Clase constructora
+class Mes {
+    constructor(sueldo, alquiler, comida, cuentas, empleados, nombreMes) {
+        this.ingreso = sueldo;
+        this.egresos = {
+            alquiler,
+            comida,
+            cuentas,
+            empleados
+        };
+        this.mes = nombreMes;
     }
+    calcularDiferencia() {
+        return this.ingreso - calcularGastos(this);
+    }
+}
+
+//Array donde se almacenan los meses
+const meses = [];
+
+//Funcion para almacenar los datos en sus respectivos meses de los cuatrimestres
+function crearMeses() {
+    // Primer cuatrimestre
+    meses.push(
+        new Mes(sueldoPrimerCuatrimestre, alquilerPrimeraMitad, comidaPrimeraMitad, cuentas, empleados, "enero"),
+        new Mes(sueldoPrimerCuatrimestre, alquilerPrimeraMitad, comidaPrimeraMitad, cuentas, empleados, "febrero"),
+        new Mes(sueldoPrimerCuatrimestre, alquilerPrimeraMitad, comidaPrimeraMitad, cuentas, empleados, "marzo"),
+
+    );
+
+    // Segundo cuatrimestre
+    meses.push(
+        new Mes(sueldoSegundoCuatrimestre, alquilerPrimeraMitad, comidaPrimeraMitad, cuentas, empleados, "abril"),
+        new Mes(sueldoSegundoCuatrimestre, alquilerPrimeraMitad, comidaPrimeraMitad, cuentas, empleados, "mayo"),
+        new Mes(sueldoSegundoCuatrimestre, alquilerPrimeraMitad, comidaPrimeraMitad, cuentas, empleados, "junio"),
+
+    );
+
+    // Tercer cuatrimestre
+    meses.push(
+        new Mes(sueldoTercerCuatrimestre, alquilerSegundaMitad, comidaSegundaMitad, cuentas, empleados, "julio"),
+        new Mes(sueldoTercerCuatrimestre, alquilerSegundaMitad, comidaSegundaMitad, cuentas, empleados, "agosto"),
+        new Mes(sueldoTercerCuatrimestre, alquilerSegundaMitad, comidaSegundaMitad, cuentas, empleados, "septiembre"),
+
+    );
+
+    // Cuarto cuatrimestre
+    meses.push(
+        new Mes(sueldoCuartoCuatrimestre, alquilerSegundaMitad, comidaSegundaMitad, cuentas, gastosVariables, "octubre"),
+        new Mes(sueldoCuartoCuatrimestre, alquilerSegundaMitad, comidaSegundaMitad, cuentas, gastosVariables, "noviembre"),
+        new Mes(sueldoCuartoCuatrimestre, alquilerSegundaMitad, comidaSegundaMitad, cuentas, gastosVariables, "diciembre"),
+    );
+}
+
+// Buscar meses segun el sueldo
+function buscarMeses(ganancia, valor) {
+    return meses.filter(mes => mes.ingreso >= valor);
+}
+
+// Calcular los gastos totales de un mes
+function calcularGastos(mes) {
+    const { alquiler, comida, cuentas, empleados } = mes.egresos;
+    return alquiler + comida + cuentas + empleados;
+}
+
+// Llamar a la funcion para crear los meses
+crearMeses();
+
+// Ejemplo de buscarMeses
+const mesesSueldo200000 = buscarMeses("ganancia", 1210000);
+console.log("Meses con ganancia de 1200000 o más:", mesesganacias1200000);
+
+// Ejemplo de calcularGastos para un mes especifico, en este caso enero
+const gastosEnero = calcularGastos(meses[0]);
+console.log("Gastos totales de enero:", gastosEnero);
+
+function diferenciaCuatrimestre(mesesCuatrimestre) {
+    let ingresosTotal = mesesCuatrimestre.reduce((total, mes) => total + mes.ingreso, 0);
+    let egresosTotal = mesesCuatrimestre.reduce((total, mes) => total + calcularGastos(mes), 0);
+    return ingresosTotal - egresosTotal;
+}
+
+// Diferencia de los cuatrimestres
+const primerCuatrimestre = meses.slice(0, 3);
+const diferenciaPrimerCuatrimestre = diferenciaCuatrimestre(primerCuatrimestre);
+console.log("Diferencia del primer cuatrimestre:", diferenciaPrimerCuatrimestre);
+
+const segundoCuatrimestre = meses.slice(3, 6);
+const diferenciaSegundoCuatrimestre = diferenciaCuatrimestre(segundoCuatrimestre);
+console.log("Diferencia del segundo cuatrimestre:", diferenciaSegundoCuatrimestre);
+
+const tercerCuatrimestre = meses.slice(6, 9);
+const diferenciaTercerCuatrimestre = diferenciaCuatrimestre(tercerCuatrimestre);
+console.log("Diferencia del tercer cuatrimestre:", diferenciaTercerCuatrimestre);
+
+const cuartoCuatrimestre = meses.slice(9, 12);
+const diferenciaCuartoCuatrimestre = diferenciaCuatrimestre(cuartoCuatrimestre);
+console.log("Diferencia del cuarto cuatrimestre:", diferenciaCuartoCuatrimestre);
+
+// Diferencia de cada mes
+meses.forEach(mes => {
+    const diferenciaMes = mes.calcularDiferencia();
+    console.log(`Diferencia de ${mes.mes}:`, diferenciaMes);
+});
